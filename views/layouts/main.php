2ac8a37e8b09ed8051795ae7f316b52cb7458b80
Okky Roy Dirgantara <okkyroydirgantara@gmail.com>
2022-04-05 06:45:45 +0700

    Display Data for News

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -52,7 +52,8 @@ AppAsset::register($this);
                     )
                     . Html::endForm()
                     . '</li>'
-                )
+                ),
+                Yii::$app->user->isGuest ? ('') : (['label' => 'Admin', 'url' => ['/admin']]),
             ],
         ]);
         NavBar::end();
--- views/site/news-list.php
+++ views/site/news-list.php
@@ -1,20 +1,25 @@
 <?php
 
 use app\models\NewsModel;
+use yii\bootstrap5\LinkPager;
 use yii\grid\ActionColumn;
 use yii\grid\GridView;
 use yii\helpers\Url;
+use yii\widgets\ListView;
 
 ?>
-<?= GridView::widget([
-    'dataProvider' => $dataProvider,
-    'columns' => [
-        ['class' => 'yii\grid\SerialColumn'],
-
-        'id',
-        'title',
-        'content:ntext',
-        'date',
-        'image',
-    ],
-]); ?>
+<div class="site-index">
+    <div class="body-content">
+        <?php echo ListView::widget([
+            'dataProvider' => $dataProvider,
+            'layout' => '{summary}<div class="row">{items}</div>{pager}',
+            'itemView' => '_news_item',
+            'itemOptions' => [
+                'class' => 'col-lg-4 col-md-6 mb-4 product-item'
+            ],
+            'pager' => [
+                'class' => LinkPager::class
+            ]
+        ]) ?>
+    </div>
+</div>
