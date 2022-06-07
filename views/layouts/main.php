5320b3eb66e0eb110a26902755c67513680784c4
OkkyRoyDirgantara <okkyroydirgantara@gmail.com>
2022-06-08 02:10:39 +0700

    Add Theme

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -5,12 +5,12 @@
 
 use app\assets\AppAsset;
 use app\widgets\Alert;
-use yii\bootstrap5\Breadcrumbs;
-use yii\bootstrap5\Html;
-use yii\bootstrap5\Nav;
-use yii\bootstrap5\NavBar;
+use yii\bootstrap4\Breadcrumbs;
+use yii\bootstrap4\Html;
+use yii\bootstrap4\Nav;
+use yii\bootstrap4\NavBar;
 
-AppAsset::register($this);
+
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
